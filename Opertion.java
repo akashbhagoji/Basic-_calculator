f0381186ab3320020b778d5213bda5586151aa22
Akash GB <01fe22bca155@kletech.ac.in>
2024-01-24 18:07:35 +0530

    Updated the code changes related  to square cube and modulus ops

---
+++ Opertion.java
@@ -0,0 +1,17 @@
+
+public class Opertion {
+    // Returns the square of a number
+    float square(float opr1) {
+        return (opr1 * opr1);
+    }
+
+    // Returns the cube of a number
+    float cube(float opr1) {
+        return (opr1 * opr1 * opr1);
+    }
+
+    // Returns the modulus of two numbers
+    float modulus(float opr1, float opr2) {
+        return (opr1 % opr2);
+    }
+}
